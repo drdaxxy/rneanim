4bc1745dee5a49c20fa6a0beaa920dc0c0939bc7
Niklas K <drdaxxy@googlemail.com>
2018-05-13 08:55:38 +0200

    background picture

--- js/RNEModelLoader.js
+++ js/RNEModelLoader.js
@@ -208,7 +208,7 @@ RNEModelLoader.prototype = {
             mesh.material.side = THREE.DoubleSide;
             mesh.material.transparent = true;
             // TODO do we need to manually copy this for decals?
-            mesh.material.outlineParameters = { alpha: 0.2 };
+            mesh.material.outlineParameters = { alpha: 0.6 };
             if (colorMapId > -1) {
                 mesh.material.map = textures[colorMapId];
             }
